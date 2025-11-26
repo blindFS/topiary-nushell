2eca6dd48cc2b4cb879fcecc5fc06fc0cd38c26a
blindfs <blindfs19@gmail.com>
2025-11-26 11:13:16 +0800

    fix(nix): persist config files against nix-collect-garbage

--- package.nix
+++ package.nix
@@ -1,6 +1,7 @@
 {
   stdenv,
   lib,
+  pkgs,
   writeShellApplication,
   tree-sitter-nu ? fetchGit "https://github.com/nushell/tree-sitter-nu",
   topiary,
@@ -8,52 +9,76 @@
   writeText,
   callPackage,
 }:
-writeShellApplication (let
-  libtree-sitter-nu = callPackage ({
-    lib,
-    stdenv,
-  }:
-    stdenv.mkDerivation (finalAttrs: {
-      pname = "tree-sitter-nu";
-      version = tree-sitter-nu.rev;
-
-      src = tree-sitter-nu;
-
-      makeFlags = [
-        # The PREFIX var isn't picking up from stdenv.
-        "PREFIX=$(out)"
-      ];
-
-      meta = with lib; {
-        description = "A tree-sitter grammar for nu-lang, the language of nushell";
-        homepage = "https://github.com/nushell/tree-sitter-nu";
-        license = licenses.mit;
-      };
-    })) {};
-in {
-  name = "topiary-nushell";
-  runtimeInputs = [nushell topiary];
-  runtimeEnv = let
-    extension = with stdenv;
-      if isLinux
-      then ".so"
-      else if isDarwin
-      then ".dylib"
-      else throw "Unsupported system: ${system}";
-  in {
-    TOPIARY_CONFIG_FILE = writeText "languages.ncl" ''
+writeShellApplication (
+  let
+    libtree-sitter-nu = callPackage (
       {
-        languages = {
-          nu = {
-            extensions = ["nu"],
-            grammar.source.path = "${libtree-sitter-nu}/lib/libtree-sitter-nu${extension}",
+        lib,
+        stdenv,
+      }:
+      stdenv.mkDerivation (finalAttrs: {
+        pname = "tree-sitter-nu";
+        version = tree-sitter-nu.rev;
+
+        src = tree-sitter-nu;
+
+        makeFlags = [
+          # The PREFIX var isn't picking up from stdenv.
+          "PREFIX=$(out)"
+        ];
+
+        meta = with lib; {
+          description = "A tree-sitter grammar for nu-lang, the language of nushell";
+          homepage = "https://github.com/nushell/tree-sitter-nu";
+          license = licenses.mit;
+        };
+      })
+    ) { };
+
+    extension =
+      with stdenv;
+      if isLinux then
+        ".so"
+      else if isDarwin then
+        ".dylib"
+      else
+        throw "Unsupported system: ${system}";
+
+    # Create a directory holding ALL runtime config files
+    # This makes a single path for GC root.
+    topiaryConfigDir = pkgs.runCommand "topiary-nushell-config" { } ''
+      local_config_dir="$out"
+
+      mkdir -p $local_config_dir/languages
+
+      # 1. Copy the nu.scm language directory
+      cp -r ${./languages/nu.scm} $local_config_dir/languages/nu.scm
+
+      # 2. Write the languages.ncl file, referencing the compiled grammar
+      cat ${writeText "languages.ncl" ''
+        {
+          languages = {
+            nu = {
+              extensions = ["nu"],
+              grammar.source.path = "${libtree-sitter-nu}/lib/libtree-sitter-nu${extension}",
+            },
           },
-        },
-      }
+        }
+      ''} > $local_config_dir/languages.ncl
     '';
-    TOPIARY_LANGUAGE_DIR = ./languages;
-  };
-  text = ''
-    ${lib.getExe topiary} "$@"
-  '';
-})
+  in
+  {
+    name = "topiary-nushell";
+    runtimeInputs = [
+      nushell
+      topiary
+    ];
+    runtimeEnv = {
+      TOPIARY_CONFIG_FILE = "${topiaryConfigDir}/languages.ncl";
+      TOPIARY_LANGUAGE_DIR = "${topiaryConfigDir}/languages";
+    };
+    text = ''
+      ${lib.getExe topiary} "$@"
+    '';
+  }
+)
