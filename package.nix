1a1ca49d7e20b6fe470a734cc037ba21d0342803
zc he <blindfs19@gmail.com>
2025-11-26 12:12:09 +0800

    fix(nix): persist config files against nix-collect-garbage (#40)
    * fix(nix): persist config files against nix-collect-garbage
    
    * cleanup
    
    * arg
    
    * refactor: use symlinkJoin instead of runCommand
    
    ---------
    
    Co-authored-by: Notarin <424c414e4b@gmail.com>

--- package.nix
+++ package.nix
@@ -7,53 +7,76 @@
   nushell,
   writeText,
   callPackage,
+  symlinkJoin,
 }:
-writeShellApplication (let
-  libtree-sitter-nu = callPackage ({
-    lib,
-    stdenv,
-  }:
-    stdenv.mkDerivation (finalAttrs: {
-      pname = "tree-sitter-nu";
-      version = tree-sitter-nu.rev;
-
-      src = tree-sitter-nu;
-
-      makeFlags = [
-        # The PREFIX var isn't picking up from stdenv.
-        "PREFIX=$(out)"
-      ];
-
-      meta = with lib; {
-        description = "A tree-sitter grammar for nu-lang, the language of nushell";
-        homepage = "https://github.com/nushell/tree-sitter-nu";
-        license = licenses.mit;
-      };
-    })) {};
-in {
+symlinkJoin {
   name = "topiary-nushell";
-  runtimeInputs = [nushell topiary];
-  runtimeEnv = let
-    extension = with stdenv;
-      if isLinux
-      then ".so"
-      else if isDarwin
-      then ".dylib"
-      else throw "Unsupported system: ${system}";
-  in {
-    TOPIARY_CONFIG_FILE = writeText "languages.ncl" ''
+  paths = [
+    (writeShellApplication (
+      let
+        libtree-sitter-nu = callPackage (
+          {
+            lib,
+            stdenv,
+          }:
+          stdenv.mkDerivation (finalAttrs: {
+            pname = "tree-sitter-nu";
+            version = tree-sitter-nu.rev;
+
+            src = tree-sitter-nu;
+
+            makeFlags = [
+              # The PREFIX var isn't picking up from stdenv.
+              "PREFIX=$(out)"
+            ];
+
+            meta = with lib; {
+              description = "A tree-sitter grammar for nu-lang, the language of nushell";
+              homepage = "https://github.com/nushell/tree-sitter-nu";
+              license = licenses.mit;
+            };
+          })
+        ) { };
+
+        extension =
+          with stdenv;
+          if isLinux then
+            ".so"
+          else if isDarwin then
+            ".dylib"
+          else
+            throw "Unsupported system: ${system}";
+      in
       {
-        languages = {
-          nu = {
-            extensions = ["nu"],
-            grammar.source.path = "${libtree-sitter-nu}/lib/libtree-sitter-nu${extension}",
-          },
-        },
+        name = "topiary-nushell";
+        runtimeInputs = [
+          nushell
+          topiary
+        ];
+        runtimeEnv = {
+          TOPIARY_CONFIG_FILE = writeText "languages.ncl" ''
+            {
+              languages = {
+                nu = {
+                  extensions = ["nu"],
+                  grammar.source.path = "${libtree-sitter-nu}/lib/libtree-sitter-nu${extension}",
+                },
+              },
+            }
+          '';
+          TOPIARY_LANGUAGE_DIR = ./languages;
+        };
+        text = ''
+          ${lib.getExe topiary} "$@"
+        '';
       }
-    '';
-    TOPIARY_LANGUAGE_DIR = ./languages;
+    ))
+    ./.
+  ];
+  meta = with lib; {
+    description = "topiary formatter for nushell";
+    homepage = "https://github.com/blindFS/topiary-nushell";
+    license = licenses.mit;
+    mainProgram = "topiary-nushell";
   };
-  text = ''
-    ${lib.getExe topiary} "$@"
-  '';
-})
+}
