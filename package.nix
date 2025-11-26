75e819fec6a62e58a08c1c2681324d4c88edafe6
blindfs <blindfs19@gmail.com>
2025-11-26 12:02:04 +0800

    refactor: use symlinkJoin instead of runCommand
    -----------------
    
    Co-authored-by: Notarin <424c414e4b@gmail.com>

--- package.nix
+++ package.nix
@@ -1,81 +1,82 @@
 {
   stdenv,
   lib,
-  runCommand,
   writeShellApplication,
   tree-sitter-nu ? fetchGit "https://github.com/nushell/tree-sitter-nu",
   topiary,
   nushell,
+  writeText,
   callPackage,
+  symlinkJoin,
 }:
-writeShellApplication (
-  let
-    libtree-sitter-nu = callPackage (
+symlinkJoin {
+  name = "topiary-nushell";
+  paths = [
+    (writeShellApplication (
+      let
+        libtree-sitter-nu = callPackage (
+          {
+            lib,
+            stdenv,
+          }:
+          stdenv.mkDerivation (finalAttrs: {
+            pname = "tree-sitter-nu";
+            version = tree-sitter-nu.rev;
+
+            src = tree-sitter-nu;
+
+            makeFlags = [
+              # The PREFIX var isn't picking up from stdenv.
+              "PREFIX=$(out)"
+            ];
+
+            meta = with lib; {
+              description = "A tree-sitter grammar for nu-lang, the language of nushell";
+              homepage = "https://github.com/nushell/tree-sitter-nu";
+              license = licenses.mit;
+            };
+          })
+        ) { };
+
+        extension =
+          with stdenv;
+          if isLinux then
+            ".so"
+          else if isDarwin then
+            ".dylib"
+          else
+            throw "Unsupported system: ${system}";
+      in
       {
-        lib,
-        stdenv,
-      }:
-      stdenv.mkDerivation (finalAttrs: {
-        pname = "tree-sitter-nu";
-        version = tree-sitter-nu.rev;
-
-        src = tree-sitter-nu;
-
-        makeFlags = [
-          # The PREFIX var isn't picking up from stdenv.
-          "PREFIX=$(out)"
+        name = "topiary-nushell";
+        runtimeInputs = [
+          nushell
+          topiary
         ];
-
-        meta = with lib; {
-          description = "A tree-sitter grammar for nu-lang, the language of nushell";
-          homepage = "https://github.com/nushell/tree-sitter-nu";
-          license = licenses.mit;
+        runtimeEnv = {
+          TOPIARY_CONFIG_FILE = writeText "languages.ncl" ''
+            {
+              languages = {
+                nu = {
+                  extensions = ["nu"],
+                  grammar.source.path = "${libtree-sitter-nu}/lib/libtree-sitter-nu${extension}",
+                },
+              },
+            }
+          '';
+          TOPIARY_LANGUAGE_DIR = ./languages;
         };
-      })
-    ) { };
-
-    extension =
-      with stdenv;
-      if isLinux then
-        ".so"
-      else if isDarwin then
-        ".dylib"
-      else
-        throw "Unsupported system: ${system}";
-
-    # Create a directory holding ALL runtime config files
-    # This makes a single path for GC root.
-    topiaryConfigDir = runCommand "topiary-nushell-config" { } ''
-      local_config_dir="$out"
-
-      # 1. Copy the nu.scm language directory
-      mkdir -p $local_config_dir/languages
-      cp ${./languages/nu.scm} $local_config_dir/languages/nu.scm
-
-      cat > $local_config_dir/languages.ncl <<EOF
-      {
-        languages = {
-          nu = {
-            extensions = ["nu"],
-            grammar.source.path = "${libtree-sitter-nu}/lib/libtree-sitter-nu${extension}",
-          },
-        },
+        text = ''
+          ${lib.getExe topiary} "$@"
+        '';
       }
-      EOF
-    '';
-  in
-  {
-    name = "topiary-nushell";
-    runtimeInputs = [
-      nushell
-      topiary
-    ];
-    runtimeEnv = {
-      TOPIARY_CONFIG_FILE = "${topiaryConfigDir}/languages.ncl";
-      TOPIARY_LANGUAGE_DIR = "${topiaryConfigDir}/languages";
-    };
-    text = ''
-      ${lib.getExe topiary} "$@"
-    '';
-  }
-)
+    ))
+    ./.
+  ];
+  meta = with lib; {
+    description = "topiary formatter for nushell";
+    homepage = "https://github.com/blindFS/topiary-nushell";
+    license = licenses.mit;
+    mainProgram = "topiary-nushell";
+  };
+}
